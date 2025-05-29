9465b75159f7d11273d2adbc584a96fb969473e4
Manan Pandey <pandeymanan024@gmail.com>
2025-05-28 20:13:53 -0400

    commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Ensure trailing slashes are handled correctly
+  trailingSlash: true,
+}
 
-// module.exports = nextConfig
+module.exports = nextConfig
