1c8e29df91789a915c841e3c452ec4d6cf35a58f
Manan Pandey <pandeymanan024@gmail.com>
2025-06-16 16:44:34 -0400

    commit

---
+++ app/privacy/page.js
@@ -0,0 +1,8 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="privacy-container">
+        <h1>Privacy Policy</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  );
+} 
---
+++ app/terms/page.js
@@ -0,0 +1,8 @@
+export default function TermsAndConditions() {
+  return (
+    <div className="terms-container">
+        <h1>Terms and Conditions</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  );
+} 
